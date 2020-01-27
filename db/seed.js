8e9af3be4f9c09e0ae3c964ef2d5e594e2875d4d
magbee <david.magbee@me.com>
2020-01-27 15:00:30 -0500

    seed start, incomplete for data structure

--- db/seed.js
+++ db/seed.js
@@ -1,3 +1,9 @@
 const seedData = require('./dev-seed.json');
+const Grid = require('../models/Grid');
 
-const 
+Grid.deleteMany({}).then(() => {
+    Grid.create(seedData).then(grids => {
+        console.log(grids);
+        process.exit();
+    })
+})
