ca2b84fb1ace7ccbb1dccf266ec061a67d92b34b
magbee <david.magbee@me.com>
2020-01-27 12:11:50 -0500

    repo cleanup

--- backend/db/connection.js
+++
@@ -1,7 +0,0 @@
-// Specify the backend mongodb connection string
-
-const mongoose = require('mongoose')
-mongoose.Promise = Promise
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/project3'
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
-module.exports = mongoose
