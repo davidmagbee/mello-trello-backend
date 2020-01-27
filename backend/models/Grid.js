f4144fd34aeb5960eaf35c2ee3607a9b0ced0eb0
Mike Smith <smithmichaels@johndeere.com>
2020-01-27 10:44:23 -0600

    Grid Updates

--- backend/models/Grid.js
+++ backend/models/Grid.js
@@ -1,62 +1,15 @@
-// Model schema for project 3
+const mongoose = require('../db/connection');
+const Schema = mongoose.Schema;
 
-const mongoose = require('../db/connection')
+const GridSchema = new Schema({
+boardName: String,
+boardDescription: String,
+boardLists: {
+type: Schema.Types.ObjectId,
+ref: "List"
+}
+});
 
-const assignedToSchema = new mongoose.Schema({
-    owner: String
-})
+const Grid = mongoose.model('Grid', GridSchema);
 
-const commentSchema = new mongoose.Schema({
-    commentorName: String,
-    comment: String
-})
-
-const taskSchema = new mongoose.Schema({
-    name: String,
-    assignedTo: [assignedToSchema],
-    dueDate: String,
-    lastModified: { type: Date, default: Date.now},
-    description: String,
-    priority: {
-        type: Number,
-        min: 1,
-        max: 5,
-        default: 5        
-     },
-    estimatedTime: {
-        type: Number,
-        min: 1,
-        max: 100,
-        default: 1
-     },    
-    color: String,
-    comments: [commentSchema]
-})
-
-const columnSchema = new mongoose.Schema({
-    name: String,
-    tasks: [taskSchema]
-})
-
-const gridSchema = new mongoose.Schema({
-    color: String,
-    description: String,
-    name: String,
-    columns: [columnSchema]
-})
-
-
-const Owner = mongoose.model("Owner", commentSchema)
-  module.exports = Owner
-
-const Comment = mongoose.model("Comment", commentSchema)
-module.exports = Comment
-
-const Task = mongoose.model("Task", taskSchema)
-module.exports = Task
-
-const Column = mongoose.model("Column", columnSchema)
-module.exports = Column
-
-const Grid = mongoose.model("Grid", gridSchema)
-module.exports = Grid
+module.exports = Grid;
