18d31a51cdabb55a8055f01eaddab0bf52550d7b
magbee <david.magbee@me.com>
2020-01-28 13:54:41 -0500

    index.js controllers update

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ const express = require('express');
 const app = express();
 
 const gridController = require('./controllers/grid-controller');
+const columnController = require('./controllers/column-controller');
+const taskController = require('./controllers/task-controller');
+const commentController = require('./controllers/comment-controller');
 
 app.use('/grid', gridController);
+app.use('/column', columnController);
+app.use('/task', taskController);
+app.use('/comment', commentController);
 
 app.get('/', (req, res) => {
     res.redirect('/grid');
 });
 
 app.use('/grid', gridController);
+app.use('/column', columnController);
+app.use('/task', taskController);
+app.use('/comment', commentController);
 
 app.set('port', process.env.PORT || 5000);
 
