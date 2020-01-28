f260e71dac1fb322d057e9e92f013ca2a27b784e
magbee <david.magbee@me.com>
2020-01-27 20:02:57 -0500

    minor syntax formatting, controller and route test

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // App startup and routing
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
-app.listen(5000, () => console.log("Yo yo, up and running on 5000!"))
+const gridController = require('./controllers/grid-controller');
+
+app.use('/grid', gridController);
+
+app.get('/', (req, res) => {
+    res.redirect('/grid');
+});
+
+app.listen(5000, () => console.log("Yo yo, up and running on 5000!"));
