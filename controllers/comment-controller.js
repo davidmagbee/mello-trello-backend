a707d44c96065d05b0dc9ef993bbd36e4918db42
magbee <david.magbee@me.com>
2020-01-29 12:39:11 -0500

    refactor to embed structure

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,31 +1,62 @@
 const express = require("express");
 const router = express.Router();
 
+const Grid = require("../models/Grid");
+const Column = require("../models/Column");
+const Task = require("../models/Task");
 const Comment = require("../models/Comment");
 
-router.get("/", (req, res) => {
-  Comment.find().then(comment => res.json(comment));
-});
+// Only need Create & Read on Comments... cleanup other routes!
+// router.get("/", (req, res) => {
+//   Comment.find().then(comment => res.json(comment));
+// });
 
-router.post("/", (req, res) => {
-  Comment.create(req.body).then(comment => {
-    res.json(comment);
+router.get("/:gridName/:columnName/:taskName", (req, res) => {
+  Grid.findOne({ gridName: req.params.gridName }).then(() => {
+    Column.findOne({ columnName: req.params.columnName }).then(() => {
+      Task.findOne({ taskName: req.params.taskName }).then(comment => {
+        res.json(comment);
+      });
+    });
   });
 });
 
-router.put("/:id", (req, res) => {
-  Comment.findByIdAndUpdate({ _id: req.params.id }, req.body, {
-    new: true
-  }).then(comment => {
-    comment.save();
-    res.json(column);
+// router.post("/", (req, res) => {
+//   Comment.create(req.body).then(comment => {
+//     res.json(comment);
+//   });
+// });
+
+router.post("/:gridName/:columnName/:taskName", (req, res) => {
+  Grid.findOne({ gridName: req.params.gridName }).then(() => {
+    Column.findOne({ columnName: req.params.columnName }).then(() => {
+      Task.findOne({ taskName: req.params.taskName }).then(() => {
+        Comment.create(req.body)
+          .then(comment => {
+            Grid.columns.tasks.push(comment);
+          })
+          .then(() => {
+            grid.save();
+            res.json(comment);
+          });
+      });
+    });
   });
 });
 
-router.delete("/:id", (req, res) => {
-  Comment.findByIdAndRemove({ _id: req.params.id }).then(comment =>
-    res.json(comment)
-  );
-});
+// router.put("/:id", (req, res) => {
+//   Comment.findByIdAndUpdate({ _id: req.params.id }, req.body, {
+//     new: true
+//   }).then(comment => {
+//     comment.save();
+//     res.json(column);
+//   });
+// });
+
+// router.delete("/:id", (req, res) => {
+//   Comment.findByIdAndRemove({ _id: req.params.id }).then(comment =>
+//     res.json(comment)
+//   );
+// });
 
 module.exports = router;
--- models/Grid.js
+++ models/Grid.js
@@ -1,16 +1,36 @@
 const mongoose = require('../db/connection');
 const Schema = mongoose.Schema;
 
-const GridSchema = new Schema({
+const commentSchema = new Schema({
+  commenterName: String,
+  comment: String
+});
+
+const taskSchema = new Schema({
+  taskName: String,
+  assignedTo: String,
+  dueDate: String,
+  lastModified: String,
+  taskDescription: String,
+  taskPriority: String,
+  estimatedTime: String,
+  color: String,
+  comments: [commentSchema]
+});
+
+const columnSchema = new Schema({
+  columnName: String,
+  columnDescription: String,
+  tasks: [taskSchema]
+});
+
+const gridSchema = new Schema({
   gridName: String,
   color: String,
   gridDescription: String,
-  columns: {
-    type: Schema.Types.ObjectId,
-    ref: 'Column'
-  }
+  columns: [columnSchema]
 });
 
-const Grid = mongoose.model('Grid', GridSchema);
+const Grid = mongoose.model('Grid', gridSchema);
 
 module.exports = Grid;
