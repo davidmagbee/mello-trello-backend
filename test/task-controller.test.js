8189d80b2a271e39b320ee02af0a910f1d81d3e1
svkalvakolanu <skalvakolanu@gmail.com>
2020-01-31 00:59:20 -0500

    all tests working and non flakey

--- test/task-controller.test.js
+++ test/task-controller.test.js
@@ -3,107 +3,98 @@ const expect = require("chai").expect;
 const supertest = require("supertest");
 const api = supertest("http://localhost:5000");
 
-// describe("GET /task", () => {
-//   it("should return a 200 response", done => {
-//     api
-//       .get("/tasks")
-//       .set("Accept", "application/json")
-//       .expect(200, done);
-//   });
-//   it("should return an array", done => {
-//     api
-//       .get("/tasks")
-//       .set("Accept", "application/json")
-//       .end((err, res) => {
-//         expect(res.body).to.be.an.apply("array");
-//       });
-//   });
-//   it("should return an array of objects with with name and description", done => {
-//     api
-//       .get("/tasks")
-//       .set("Accept", "application/json")
-//       .end((err, res) => {
-//         expect(res.body[0].to.have.property("taskName"));
-//         done();
-//       });
-//   });
-// });
-
 describe("POST /tasks", () => {
-    before(done => {
-        api
-            .post("/tasks")
-            .set("Accept", "application/json")
-            .send({
-                taskName: "meet MVP",
-                taskDescription: "MVP needs"
-            })
-            .end(done)
-    })
-    it("should add a new task to the array", done => {
-        api
-            .get("/grids")
-            .set("Accept", "application/json")
-            .end((err, res) => {
-                expect(res.body[0].tasks.length).to.equal(1)
-            })
-        api
-            .delete("/tasks/meet mvp")
-            .set("Accept", "application/json")
-            .end(done);
-    })
+  before(done => {
+    api
+      .get("/grids")
+      .set("Accept", "application/json")
+      .end((err, res) => {
+        gridToPostTo = res.body[0]._id;
+        done();
+      });
+  });
+
+  before(done => {
+    api
+      .post(`/tasks/${gridToPostTo}`)
+      .set("Accept", "application/json")
+      .send({
+        taskName: "meet MVP",
+        taskDescription: "MVP needs"
+      })
+      .end(done);
+  });
+  it("should add a new task to the array", done => {
+    api
+      .get("/grids")
+      .set("Accept", "application/json")
+      .end((err, res) => {
+        expect(res.body[0].tasks.length).to.equal(2);
+        done()
+      });
+});
 })
 
-// describe("PUT /grids/tasks/:tId", () => {
-//     before(done => {
-//         api
-//             .put("/grids/5e333a144ec47a0c2435ba5a/5e333a144ec47a0c2435ba59/")
-//             .set("Accept", "application/json")
-//             .send({
-//                 taskName: "meet MVP",
-//                 taskDescription: "task PUT test",
-//             })
-//             .end(done)
-//     })
-//     it("should update existing task", done => {
-//         api
-//             .get("/grids/5e333a144ec47a0c2435ba5a/._id")
-//             .set("Accept", "application/json")
-//             .end((err, res) => {
-//                 expect(res.body.taskDescription).to.equal("task PUT test")
-//             })
-//         api
-//             .put("/grids/5e333a144ec47a0c2435ba5a/5e333a144ec47a0c2435ba59")
-//             .set("Accept", "application/json")
-//             .send({
-//                 taskName: "example Task",
-//                 taskDescription: "lorem ipsum",
-//             })
-//             .end(done)
-//     })
-// })
 
-describe("DELETE /tasks/example task", () => {
-    before(done => {
-        api
-            .delete("/grids/5e333a144ec47a0c2435ba5a/5e333a144ec47a0c2435ba59")
-            .set("Accept", "application/json")
-            .end(done);
-    })
-    it("should remove a task from the object", done => {
-        api
-            .get("/grids/5e333a144ec47a0c2435ba5a/5e333a144ec47a0c2435ba59")
-            .set("Accept", "application/json")
-            // .end((err, res) => {
-            //     expect(res.body).to.be.undefined
-            // })
-            // api
-            // .post("/grids/5e333a144ec47a0c2435ba5a/5e333a144ec47a0c2435ba59")
-            // .set("Accept", "application/json")
-            // .send({
-            //     taskName: "example Task",
-            //     taskDescription: "lorem ipsum"
-            // })
-            .end(done)
-    })
+describe("PUT /tasks", () => {
+  before(done => {
+    api
+      .get("/grids")
+      .set("Accept", "application/json")
+      .end((err, res) => {
+        gridToUpdate = res.body[0]._id;
+        taskToUpdate= res.body[0].tasks[1]._id
+        done();
+      });
+  });
+
+  before(done => {
+    api
+      .put(`/tasks/${gridToPostTo}/${taskToUpdate}`)
+      .set("Accept", "application/json")
+      .send({
+        taskName: "put test",
+        taskDescription: "put test"
+      })
+      .end(done);
+  });
+  it("should add a new task to the array", done => {
+    api
+      .get("/grids")
+      .set("Accept", "application/json")
+      .end((err, res) => {
+        expect(res.body[0].tasks[1].taskName).to.equal("put test");
+        done()
+      });
+});
+})
+
+
+describe("DELETE /tasks", () => {
+before(done => {
+    api
+      .get("/grids")
+      .set("Accept", "application/json")
+      .end((err, res) => {
+        gridToUpdate = res.body[0]._id;
+        taskToUpdate= res.body[0].tasks[1]._id
+        done();
+      });
+  });
+
+  before(done => {
+    api
+      .delete(`/tasks/${gridToPostTo}/${taskToUpdate}`)
+      .set("Accept", "application/json")
+      .end(done);
+  });
+  it("should add a new task to the array", done => {
+    api
+      .get("/grids")
+      .set("Accept", "application/json")
+      .end((err, res) => {
+        expect(res.body[0].tasks.length).to.equal(1);
+        done()
+      });
+});
 })
