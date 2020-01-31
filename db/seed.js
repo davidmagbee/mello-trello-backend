043ea635d7c7f677aa15b3e1ec52d93b26321071
magbee <david.magbee@me.com>
2020-01-31 12:17:03 -0500

    seed file adjustment to on grid

--- db/seed.js
+++ db/seed.js
@@ -6,25 +6,14 @@ const Task = require("../models/Task");
 Grid.deleteMany({}).then(() => {
   Task.deleteMany({}).then(() => {
     Task.create({
-      taskName: "Utilize MERN stack",
-      taskDescription: "Integrate Mongo Atlas"
+      taskName: "Scoring functionality",
+      taskDescription: "Score whether the user's guess is correct or not."
     }).then(task => {
       Grid.create({
-        gridName: "Portfolio",
+        gridName: "Python Project",
         color: "#123151",
-        gridDescription: "Revisit and refactor portfolio",
+        gridDescription: "Build a simple game using Python",
         tasks: task.id
-      });
-      Task.create({
-        taskName: "Scoring functionality",
-        taskDescription: "Score whether the user's guess is correct or not."
-      }).then(task => {
-        Grid.create({
-          gridName: "Python Project",
-          color: "#123151",
-          gridDescription: "Build a simple game using Python",
-          tasks: task.id
-        });
       });
     });
   });
